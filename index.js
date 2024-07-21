96294e7b29e713ba8fcbf2bccba06ec8b685f611
Honey-Joe <honeyjoe943@gmail.com>
2024-07-22 03:01:21 +0530

    database added

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const mess = document.getElementById("message")
 const merr = document.getElementById("messerr")
 
 
+
+
+const savetodb = (fname,lname,email,mess)=>{
+    const firebaseConfig = {
+        apiKey: "AIzaSyBMn7Yw5zTqZvVLqzSs3_HxSucbFrBF0eQ",
+        authDomain: "portfolio-8323e.firebaseapp.com",
+        databaseURL: "https://portfolio-8323e-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "portfolio-8323e",
+        storageBucket: "portfolio-8323e.appspot.com",
+        messagingSenderId: "256157583296",
+        appId: "1:256157583296:web:f55cec94db4d50ad19ea25"
+      };
+    
+    
+      // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database().ref("ReqForm");
+ 
+var newRef = db.push();
+
+newRef.set(
+  {
+    fname : fname,
+    lname:lname,
+    email : email,
+    mess : mess
+  }
+);
+
+}
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
 
@@ -28,5 +59,7 @@ form.addEventListener("submit", (e)=>{
         merr.innerHTML="*Required"
         merr.style.color="red"
     }
+
+    savetodb(fname.value,lname.value,email.value,mess.value);
 
 })
